52b2bcf77415a27e213470fa0214d8019859c3c5
MBR#0001 <mbr@mbr.pw>
2023-02-17 22:15:07 +0100

    Modernize code

--- Jellyfin.Plugin.OpenSubtitles/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.OpenSubtitles/Configuration/PluginConfiguration.cs
@@ -1,25 +1,24 @@
 using MediaBrowser.Model.Plugins;
 
-namespace Jellyfin.Plugin.OpenSubtitles.Configuration
+namespace Jellyfin.Plugin.OpenSubtitles.Configuration;
+
+/// <summary>
+/// The plugin configuration.
+/// </summary>
+public class PluginConfiguration : BasePluginConfiguration
 {
     /// <summary>
-    /// The plugin configuration.
+    /// Gets or sets the username.
     /// </summary>
-    public class PluginConfiguration : BasePluginConfiguration
-    {
-        /// <summary>
-        /// Gets or sets the username.
-        /// </summary>
-        public string Username { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
 
-        /// <summary>
-        /// Gets or sets the password.
-        /// </summary>
-        public string Password { get; set; } = string.Empty;
+    /// <summary>
+    /// Gets or sets the password.
+    /// </summary>
+    public string Password { get; set; } = string.Empty;
 
-        /// <summary>
-        /// Gets or sets the custom API Key.
-        /// </summary>
-        public string CustomApiKey { get; set; } = string.Empty;
-    }
+    /// <summary>
+    /// Gets or sets the custom API Key.
+    /// </summary>
+    public string CustomApiKey { get; set; } = string.Empty;
 }
